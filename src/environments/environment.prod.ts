f1ef54e80a3aa3cb0a0fa5df7f66ed21d651cdbb
Ashish Singh <ashish03374@gmail.com>
2025-04-16 17:17:29 +0530

    feat: added mock youtube-videos.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,8 @@ import { VideoSourceEnum } from '../app/shared/enums/enums';
 export const environment = {
   production: false,
   youTubeApiKey: '',
-  youTubeApiUrl: 'https://www.googleapis.com/youtube/v3/search',
+  youTubeApiUrl1: 'https://www.googleapis.com/youtube/v3/search',
+  youTubeApiUrl: 'https://ashishsinghparihar.github.io/youtube-videos-db.json',
   mockVideoApiUrl: 'https://ashishsinghparihar.github.io/videos-db.json',
   videoSource: VideoSourceEnum.YOUTUBE,
 };
